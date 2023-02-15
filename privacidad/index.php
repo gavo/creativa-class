302ad6c59b38d753f280a1cd2aa60a7b71d8ce4a
Gustavo Vargas Miranda <gustavo@proemex.com>
2023-02-15 03:10:40 -0400

    Agregando página de términos de uso de la plataforma

---
+++ privacidad/index.php
@@ -0,0 +1,239 @@
+<?php
+    require_once('../config.php');
+    $PAGE->set_context(context_system::instance());
+    $urlparams = array('id' => -1);
+    $url = new moodle_url('/privacidad/index.php', $urlparams);
+    $PAGE->set_url($url);
+    $PAGE->set_pagelayout('standard');
+    if($PAGE != null && $SITE != null && $SITE->fullname != null){
+        $PAGE->set_title($SITE->fullname);
+    } 
+    
+    echo $OUTPUT->header();
+    echo $PAGE->set_heading('POLÍTICA DE PRIVACIDAD');?>
+    
+    <p class=MsoNormal style='text-align:justify'>Protección de datos de carácter
+        personal según la LOPD </p>
+
+    <p class=MsoNormal style='text-align:justify'>Creativa <span class=SpellE>Class</span>,
+        en aplicación de la normativa vigente en materia de protección de datos de
+        carácter personal, informa que los datos personales que se recogen a través de
+        los formularios del Sitio web: https://creativaclass.com, se incluyen en los
+        ficheros automatizados específicos de usuarios de los servicios de Creativa <span class=SpellE>Class</span>.</p>
+
+    <p class=MsoNormal style='text-align:justify'>La recogida y tratamiento
+        automatizado de los datos de carácter personal tiene como finalidad el
+        mantenimiento de la relación comercial y el desempeño de tareas de información,
+        formación, asesoramiento y otras actividades propias del acceso a las aulas
+        virtuales de Creativa <span class=SpellE>Class</span>. Estos datos únicamente
+        serán cedidos a aquellas entidades que sean necesarias con el único objetivo de
+        dar cumplimiento a la finalidad anteriormente expuesta. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Creativa <span class=SpellE>Class</span>
+        adopta las medidas necesarias para garantizar la seguridad, integridad y
+        confidencialidad de los datos conforme a lo dispuesto en el Reglamento (UE)
+        2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, relativo
+        a la protección de las personas físicas en lo que respecta al tratamiento de
+        datos personales y a la libre circulación de los mismos. El usuario podrá en
+        cualquier momento ejercitar los derechos de acceso, oposición, rectificación y
+        cancelación reconocidos en el citado Reglamento (UE). </p>
+
+    <p class=MsoNormal style='text-align:justify'>El ejercicio de estos derechos
+        puede realizarlo el propio usuario a través de email a: <a href="mailto:info@bienaventuranza.org"><span style='color:windowtext;
+text-decoration:none;text-underline:none'>info@bienaventuranza.org</span></a> o
+        en la dirección: Bolivia/Santa Cruz/Santa Cruz de la Sierra/ <span class=SpellE>Av</span> <span
+            class=SpellE>Cordecruz</span> <span class=SpellE>nro</span>
+        45. </p>
+
+    <p class=MsoNormal style='text-align:justify'>El usuario manifiesta que todos
+        los datos facilitados por él son ciertos y correctos, y se compromete a
+        mantenerlos actualizados, comunicando los cambios a Creativa <span class=SpellE>Class</span></p>
+
+    <p class=MsoNormal style='text-align:justify'>
+        <o:p>&nbsp;</o:p>
+    </p>
+
+    <p class=MsoNormal style='text-align:justify'><b>Finalidad del tratamiento de
+            los datos personales: <o:p></o:p></b></p>
+
+    <p class=MsoNormal style='text-align:justify'>¿Con qué finalidad trataremos tus
+        datos personales? </p>
+
+    <p class=MsoNormal style='text-align:justify'>En Creativa <span class=SpellE>Class</span>,
+        trataremos tus datos personales recabados a través del Sitio Web: https://creativaclass.com,
+        con las siguientes finalidades: </p>
+
+    <p class=MsoNormal style='text-align:justify'>1. En caso de contratación de los
+        bienes y servicios ofertados a través de Creativa <span class=SpellE>Class</span>,
+        para mantener la relación contractual, así como la gestión, administración,
+        información, prestación y mejora del servicio. </p>
+
+    <p class=MsoNormal style='text-align:justify'>2. Remitir boletines (<span class=SpellE>newsletters</span>), así como
+        comunicaciones comerciales de
+        promociones y/o publicidad de Creativa <span class=SpellE>Class</span> y del
+        sector educativo. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Te recordamos que puedes oponerte
+        al envío de comunicaciones comerciales por cualquier vía y en cualquier
+        momento, remitiendo un correo electrónico a la dirección indicada
+        anteriormente. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Los campos de dichos registros
+        son de cumplimentación obligatoria, siendo imposible realizar las finalidades
+        expresadas si no se aportan esos datos. ¿Por cuánto tiempo se conservan los
+        datos personales recabados? Los datos personales proporcionados se conservarán
+        mientras se mantenga la relación comercial o no solicites su supresión y
+        durante el plazo por el cuál pudieran derivarse responsabilidades legales por
+        los servicios prestados. </p>
+
+    <p class=MsoNormal style='text-align:justify'><b>Legitimación: <o:p></o:p></b></p>
+
+    <p class=MsoNormal style='text-align:justify'>El tratamiento de tus datos se
+        realiza con las siguientes bases jurídicas que legitiman el mismo: </p>
+
+    <p class=MsoNormal style='text-align:justify'>1. La solicitud de información
+        y/o la contratación de los servicios de Creativa Class, cuyos
+        términos y condiciones se pondrán a tu disposición en todo caso, de forma
+        previa a una eventual contratación. </p>
+
+    <p class=MsoNormal style='text-align:justify'>2. El consentimiento libre,
+        específico, informado e inequívoco, en tanto que te informamos poniendo a tu
+        disposición la presente política de privacidad, <span class=GramE>que</span>
+        tras la lectura de la misma, en caso de estar conforme, puedes aceptar mediante
+        una declaración o una clara acción afirmativa, como el marcado de una casilla
+        dispuesta al efecto. </p>
+
+    <p class=MsoNormal style='text-align:justify'>En caso de que no nos facilites
+        tus datos o lo hagas de forma errónea o incompleta, no podremos atender tu
+        solicitud, resultando del todo imposible proporcionarte la información
+        solicitada o llevar a cabo la contratación de los servicios. </p>
+
+    <p class=MsoNormal style='text-align:justify'>
+        <o:p>&nbsp;</o:p>
+    </p>
+
+    <p class=MsoNormal style='text-align:justify'><b>Destinatarios: <o:p></o:p></b></p>
+
+    <p class=MsoNormal style='text-align:justify'>Los datos no se comunicarán a
+        ningún tercero ajeno a Creativa <span class=SpellE>Class</span>, salvo
+        obligación legal. </p>
+
+    <p class=MsoNormal style='text-align:justify'>En los casos en que el usuario
+        incluya ficheros con datos de carácter personal en los servidores de
+        alojamiento compartido, Creativa <span class=SpellE>Class</span> no se hace
+        responsable del incumplimiento por parte del usuario del RGPD. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Retención de datos en conformidad
+        a la LSSI Creativa <span class=SpellE>Class</span> informa de que, como
+        prestador de servicio de alojamiento de datos y en virtud de lo establecido en
+        la Ley 34/2002 de 11 de julio de Servicios de la Sociedad de la Información y
+        de Comercio Electrónico (LSSI), retiene por un periodo máximo de 12 meses la
+        información imprescindible para identificar el origen de los datos alojados y
+        el momento en que se inició la prestación del servicio. La retención de estos
+        datos no afecta al secreto de las comunicaciones y sólo podrán ser utilizados
+        en el marco de una investigación criminal o para la salvaguardia de la
+        seguridad pública, poniéndose a disposición de los jueces y/o tribunales o del
+        Ministerio que así los requiera. La comunicación de datos a las Fuerzas y
+        Cuerpos del Estado se hará en virtud a lo dispuesto en la normativa sobre
+        protección de datos personales. Derechos propiedad intelectual Creativa <span class=SpellE>Class</span> es
+        titular de todos los derechos de autor, propiedad
+        intelectual, industrial, &quot;<span class=SpellE>know</span> <span class=SpellE>how</span>&quot; y cuantos
+        otros derechos guardan relación con los
+        contenidos del sitio web Creativa <span class=SpellE>Class</span> y los
+        servicios ofertados en el mismo, así como de los programas necesarios para su
+        implementación y la información relacionada. </p>
+
+    <p class=MsoNormal style='text-align:justify'>No se permite la reproducción,
+        publicación y/o uso no estrictamente privado de los contenidos, totales o
+        parciales, del sitio web https://creativaclass.com sin el consentimiento previo
+        y por escrito. Propiedad intelectual del software El usuario debe respetar los
+        programas de terceros puestos a su disposición por Creativa <span class=SpellE>Class</span>,
+        aun siendo gratuitos y/o de disposición pública. Creativa <span class=SpellE>Class</span>
+        dispone de los derechos de explotación y propiedad intelectual necesarios del
+        software. El usuario no adquiere derecho alguno o licencia por el servicio
+        contratado, sobre el software necesario para la prestación del servicio, ni
+        tampoco sobre la información técnica de seguimiento del servicio, excepción
+        hecha de los derechos y licencias necesarios para el cumplimiento de los
+        servicios contratados y únicamente durante la duración de los mismos. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Para toda actuación que exceda
+        del cumplimiento del contrato, el usuario necesitará autorización por escrito
+        por parte de Creativa <span class=SpellE>Class</span>, quedando prohibido al
+        usuario acceder, modificar, visualizar la configuración, estructura y ficheros
+        de los servidores propiedad de Creativa <span class=SpellE>Class</span>,
+        asumiendo la responsabilidad civil y penal derivada de cualquier incidencia que
+        se pudiera producir en los servidores y sistemas de seguridad como consecuencia
+        directa de una actuación negligente o maliciosa por su parte. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Propiedad intelectual de los
+        contenidos alojados </p>
+
+    <p class=MsoNormal style='text-align:justify'>Se prohíbe el uso contrario a la
+        legislación sobre propiedad intelectual de los servicios prestados por Creativa
+        <span class=SpellE>Class</span> y, en particular de:
+    </p>
+
+    <p class=MsoNormal style='text-align:justify'>• La utilización que resulte
+        contraria a las leyes españolas o que infrinja los derechos de terceros. • La
+        publicación o la transmisión de cualquier contenido que, a juicio de Creativa <span class=SpellE>Class</span>,
+        resulte violento, obsceno, abusivo, ilegal, racial,
+        xenófobo o difamatorio. </p>
+
+    <p class=MsoNormal style='text-align:justify'>• Los <span class=SpellE>cracks</span>,
+        números de serie de programas o cualquier otro contenido que vulnere derechos
+        de la propiedad intelectual de terceros. </p>
+
+    <p class=MsoNormal style='text-align:justify'>• La recogida y/o utilización de
+        datos personales de otros usuarios sin su consentimiento expreso o
+        contraviniendo lo dispuesto en Reglamento (UE) 2016/679 del Parlamento Europeo
+        y del Consejo, de 27 de abril de 2016, relativo a la protección de las personas
+        físicas en lo que respecta al tratamiento de datos personales y a la libre
+        circulación de los mismos. • La utilización del servidor de correo del dominio
+        y de las direcciones de correo electrónico para el envío de correo masivo no
+        deseado. </p>
+
+    <p class=MsoNormal style='text-align:justify'>El usuario tiene toda la
+        responsabilidad sobre el contenido de su web, la información transmitida y
+        almacenada, los enlaces de hipertexto, las reivindicaciones de terceros y las
+        acciones legales en referencia a propiedad intelectual, derechos de terceros y
+        protección de menores.</p>
+
+    <p class=MsoNormal style='text-align:justify'>El usuario es responsable
+        respecto a las leyes y reglamentos en vigor y las reglas que tienen que ver con
+        el funcionamiento del servicio online, comercio electrónico, derechos de autor,
+        mantenimiento del orden público, así como principios universales de uso de
+        Internet.</p>
+
+    <p class=MsoNormal style='text-align:justify'>Protección de la información
+        alojada Creativa <span class=SpellE>Class</span> realiza copias de seguridad de
+        los contenidos alojados en sus servidores, sin <span class=GramE>embargo</span>
+        no se responsabiliza de la pérdida o el borrado accidental de los datos por
+        parte de los usuarios. De igual manera, no garantiza la reposición total de los
+        datos borrados por los usuarios, ya que los citados datos podrían haber sido
+        suprimidos y/o modificados durante el periodo del tiempo transcurrido desde la
+        última copia de seguridad. Los servicios ofertados, excepto los servicios
+        específicos de <span class=SpellE>backup</span>, no incluyen la reposición de
+        los contenidos conservados en las copias de seguridad realizadas por Creativa <span class=SpellE>Class</span>,
+        cuando esta pérdida sea imputable al usuario; en
+        este caso, se determinará una tarifa acorde a la complejidad y volumen de la
+        recuperación, siempre previa aceptación del usuario. </p>
+
+    <p class=MsoNormal style='text-align:justify'>Comunicaciones comerciales </p>
+
+    <p class=MsoNormal style='text-align:justify'>En aplicación de la LSSI. Creativa
+        <span class=SpellE>Class</span> no enviará comunicaciones publicitarias o
+        promocionales por correo electrónico u otro medio de comunicación electrónica
+        equivalente que previamente no hubieran sido solicitadas o expresamente
+        autorizadas por los destinatarios de las mismas. En el caso de usuarios con los
+        que exista una relación contractual previa, Creativa <span class=SpellE>Class</span>
+        sí está autorizado al envío de comunicaciones comerciales referentes a
+        productos o servicios de Creativa <span class=SpellE>Class</span> que sean
+        similares a los que inicialmente fueron objeto de contratación con el cliente.
+        En todo caso, el usuario, tras acreditar su identidad, podrá solicitar que no
+        se le haga llegar más información comercial a través de los canales de Atención
+        al Cliente
+    </p>
+
+<?php 
+    echo $OUTPUT->footer();
+?>
